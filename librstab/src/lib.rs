8f84e9341033ee55f07f7800060b09602fac250b
Rob van Putten <breinbaasnl@gmail.com>
2022-05-03 12:13:36 +0200

    added framework for polygon calculations

--- librstab/src/lib.rs
+++ librstab/src/lib.rs
@@ -2,6 +2,9 @@ use std::env;
 use std::fs::File;
 use std::path::PathBuf;
 
+use geo_clipper::Clipper;
+use geo_types::{Coordinate, LineString, Polygon};
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -20,12 +23,31 @@ struct Layer {
 }
 
 #[derive(Serialize, Deserialize)]
-struct Geometry {
+struct Bishop {
+    mx: f32,
+    mz: f32,
+    r: f32,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Geometry {
     layers: Vec<Layer>,
 }
 
-pub fn add_one(x: i32) -> i32 {
-    x + 1
+impl Geometry {
+    pub fn from_json_file(file_path: PathBuf) -> Geometry {
+        let file = File::open(file_path).expect("file not found");
+        let geometry: Geometry = serde_json::from_reader(file).expect("error while reading");
+        geometry
+    }
+}
+
+// struct Slice {
+//     polygon: Polygon,
+// }
+
+pub fn bishop(geometry: Geometry, mx: f32, mz: f32, r: f32) -> f32 {
+    0.1
 }
 
 #[cfg(test)]
@@ -33,17 +55,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(3, add_one(2));
-    }
-
-    #[test]
     fn read_file() {
         let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         d.push("testdata/1.json");
-        let file = File::open(d).expect("file not found");
-        let geometry: Geometry = serde_json::from_reader(file).expect("error while reading");
+        let geometry: Geometry = Geometry::from_json_file(d);
         assert_eq!(geometry.layers.len(), 1);
-        assert_eq!(geometry.layers[0].points.len(), 4);
+        assert_eq!(geometry.layers[0].points.len(), 6);
     }
 }
